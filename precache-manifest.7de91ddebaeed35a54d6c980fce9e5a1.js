c2c3af4a9020e3d7c3909ec62ab473c84c37a1a6
cjosti <cleitonosti@gmail.com>
2020-02-22 16:45:43 -0300

    Updates

---
+++ precache-manifest.7de91ddebaeed35a54d6c980fce9e5a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db23b2c4c091b417cb76d703e97099c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95126bfac35d71174513",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2a30c350efda4cf933b",
+    "url": "/static/js/2.e96ca3b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e96ca3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95126bfac35d71174513",
+    "url": "/static/js/main.5b28dce0.chunk.js"
+  },
+  {
+    "revision": "c0c04b767d62e4e72127",
+    "url": "/static/js/runtime-main.9511603b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
