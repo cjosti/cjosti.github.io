68a4a55706f2a270128f79d3cfc488d1f19e0565
cjosti <cleitonosti@gmail.com>
2020-02-22 16:55:26 -0300

    Updates

---
+++ precache-manifest.20e0101f8dcb87029815f55ea3e2bfd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ca529d60a1ac7becf9d1997ba03cde",
+    "url": "/teste/index.html"
+  },
+  {
+    "revision": "a31c485c5c5a99e9b58d",
+    "url": "/teste/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2a30c350efda4cf933b",
+    "url": "/teste/static/js/2.e96ca3b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/teste/static/js/2.e96ca3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31c485c5c5a99e9b58d",
+    "url": "/teste/static/js/main.d8efa6bf.chunk.js"
+  },
+  {
+    "revision": "5d104b37e29f291d1ca2",
+    "url": "/teste/static/js/runtime-main.c71f11f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teste/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cleitonosti.github.io/precache-manifest.8ba0a704eaf68703190f6dda9af47bbd.js"
+  "/teste/precache-manifest.20e0101f8dcb87029815f55ea3e2bfd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cleitonosti.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teste/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
